231c7414c64e25f224eebff310dad2e4167da4ac
Ichlasul Fikri <ichlasul.ap@gmail.com>
2026-02-18 21:23:25 +0700

    feat: setup config for mail service

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -6,3 +6,5 @@ export const JWT_SECRET_TOKEN = process.env.JWT_SECRET_TOKEN;
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const USER_EMAILER = process.env.USER_EMAILER;
+export const PASSWORD_EMAILER = process.env.PASSWORD_EMAILER;
